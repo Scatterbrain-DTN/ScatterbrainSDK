54187b1f9bb711656edba7a06794cf11d78cc680
Alex Ballmer <gnu3ra@riseup.net>
2022-04-30 12:15:09 -0500

    handle null results in IdentityImportContract

--- scatterbrainSDK/src/main/java/net/ballmerlabs/scatterbrainsdk/IdentityImportContract.kt
+++ scatterbrainSDK/src/main/java/net/ballmerlabs/scatterbrainsdk/IdentityImportContract.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.activity.result.contract.ActivityResultContract
 
-class IdentityImportContract : ActivityResultContract<Int, List<Identity>>() {
+class IdentityImportContract : ActivityResultContract<Int, List<Identity>?>() {
     override fun createIntent(context: Context, input: Int?): Intent {
         return Intent().apply {
             component = ScatterbrainApi.IMPORT_IDENTITY_COMPONENT
@@ -12,7 +12,7 @@ class IdentityImportContract : ActivityResultContract<Int, List<Identity>>() {
         }
     }
 
-    override fun parseResult(resultCode: Int, intent: Intent?): List<Identity> {
-        return intent!!.getParcelableArrayListExtra(ScatterbrainApi.EXTRA_IDENTITY_RESULT)!!
+    override fun parseResult(resultCode: Int, intent: Intent?): List<Identity>? {
+        return intent?.getParcelableArrayListExtra(ScatterbrainApi.EXTRA_IDENTITY_RESULT)
     }
 }
