084bf7175878e7f18ad76470b3eda58e7e4e503a
Alex Ballmer <gnu3ra@riseup.net>
2022-08-21 02:14:36 -0500

    update all gradle dependencies
    why is everyone talking about linux!@! its an operating system for dummies

--- scatterbrainSDK/src/main/java/net/ballmerlabs/scatterbrainsdk/IdentityImportContract.kt
+++ scatterbrainSDK/src/main/java/net/ballmerlabs/scatterbrainsdk/IdentityImportContract.kt
@@ -5,7 +5,7 @@ import android.content.Intent
 import androidx.activity.result.contract.ActivityResultContract
 
 class IdentityImportContract : ActivityResultContract<Int, List<Identity>?>() {
-    override fun createIntent(context: Context, input: Int?): Intent {
+    override fun createIntent(context: Context, input: Int): Intent {
         return Intent().apply {
             component = ScatterbrainApi.IMPORT_IDENTITY_COMPONENT
             putExtra(ScatterbrainApi.EXTRA_NUM_IDENTITIES, input)
